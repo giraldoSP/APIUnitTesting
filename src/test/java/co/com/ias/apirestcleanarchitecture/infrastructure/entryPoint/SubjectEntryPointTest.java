484351cc7dda00d051110edaf9e2d7c823714031
Santiago Giraldo <porqueamisg@gmail.com>
2023-01-25 21:53:30 -0500

    b

--- src/test/java/co/com/ias/apirestcleanarchitecture/infrastructure/entryPoint/SubjectEntryPointTest.java
+++ src/test/java/co/com/ias/apirestcleanarchitecture/infrastructure/entryPoint/SubjectEntryPointTest.java
@@ -5,6 +5,9 @@ import co.com.ias.apirestcleanarchitecture.domain.usecase.subject.SubjectUseCase
 import co.com.ias.apirestcleanarchitecture.infrastructure.entrypoint.subject.SubjectEntryPoint;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.tomcat.jni.File;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Java6BDDSoftAssertionsProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,8 +19,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.Assert;
+
+import java.net.http.HttpClient;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(SubjectEntryPoint.class)
@@ -50,5 +57,40 @@ public class SubjectEntryPointTest {
                 .content(mapper.writeValueAsString(subjectDTO)))
                 .andExpect(MockMvcResultMatchers.status().isCreated());
         //Assert
+
+
     }
+
+
+    @Test
+    @DisplayName("Status 200")
+    void httpResult200() throws Exception{
+
+       mockMvc.perform(MockMvcRequestBuilders.get("/subject")).
+               andExpect(MockMvcResultMatchers.status().isOk());
+
+    }
+
+    @Test
+    @DisplayName("500")
+    void ServerErrorTest() throws Exception {
+        //Arrange
+
+
+
+
+        //Act
+
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/1234565"))
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+        //Assert
+
+
+    }
+
+
+
 }
+
+
